5d176465d8755acbd2f458d919618c1e5aaa96d6
Zack Scholl <zack.scholl@gmail.com>
2022-07-04 12:28:16 -0700

    cool

--- lib/turntable.lua
+++ lib/turntable.lua
@@ -10,7 +10,9 @@ end
 
 function Turntable:oscdata(datatype,data)
   if datatype=="cursor" then
-    self.vw:cursor(data)
+    if self.ready then
+      self.vw:cursor(data)
+    end
   elseif datatype=="ready" then
     self.vw=viewwave_:new{id=self.id,path=self.path}
     self.ready=true
@@ -18,13 +20,12 @@ function Turntable:oscdata(datatype,data)
 end
 
 function Turntable:init()
+  self.ready=false
   params:add_group("table "..self.id,2)
   params:add{type='binary',name='play',id=self.id..'play',behavior='toggle',action=function(v)
     engine.set(self.id,"amp",v==1 and 1.0 or 0,params:get(self.id.."fadetime"))
   end}
   params:add_control(self.id.."fadetime","fade time",controlspec.new(0,64,'lin',0.01,1,'seconds',0.01/64))
-  self.playing=false
-  self.ready=false
   local bpm=clock.get_tempo()
   for word in string.gmatch(self.path,'([^_]+)') do
     if string.find(word,"bpm") then
